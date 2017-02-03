4036142d37229a75b2927bb5bfb7d032dcae09d7
Pablo Ligero <Pablo Ligero>
2017-02-03 19:21:49 +0100

    Again removed

--- Hello.java
+++ Hello.java
@@ -1,5 +1,5 @@
 public class Hello{
     public static void main(String args[]){
-        System.out.println("Helloooo its meeeee again");
+        System.out.println("Helloooo its meeeee");
     }
 }
